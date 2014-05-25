8f5e748813cfb4a1c749a7d12d452a6dcff524cb
enissen <e.sbz@gmx.net>
2014-05-25 12:46:12 +0200

    improving unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,14 @@
-# Set the working application directory
-# working_directory "/path/to/your/app"
 working_directory "/home/deploy/sites/miguelnager.com/current"
 
-# Unicorn PID file location
-# pid "/path/to/pids/unicorn.pid"
-pid "/home/deploy/sites/miguelnager.com/current/pids/unicorn.pid"
+pid "#{working_directory}/pids/unicorn.pid"
 
-# Path to logs
-# stderr_path "/path/to/log/unicorn.log"
-# stdout_path "/path/to/log/unicorn.log"
-stderr_path "/home/deploy/sites/miguelnager.com/current/log/unicorn.log"
-stdout_path "/home/deploy/sites/miguelnager.com/current/log/unicorn.log"
+stderr_path "#{working_directory}/log/unicorn.log"
+stdout_path "#{working_directory}/log/unicorn.log"
 
-# Unicorn socket
 listen "/tmp/unicorn.miguelnager.sock"
 listen "/tmp/unicorn.myapp.sock"
 
-# Number of processes
 # worker_processes 4
 worker_processes 2
 
-# Time-out
 timeout 30
