abcc0f323248d53ad62a591ea9d2f4e7b1b112d8
enissen <e.sbz@gmx.net>
2014-09-05 12:02:48 +0200

    require jquery.jCrop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Mnc::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.precompile += %w( polyfills.js )
 end
